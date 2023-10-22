9a126c4b1525c5dfb6fe7176e14bbd8499812f16
KitLavis <>
2023-10-22 12:00:11 +0000

    JS: Created card array

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let cardArray = [
+        {
+            name: 'coin',
+            img: 'assets/images/coin.webp'
+        },
+        {
+            name: 'coin',
+            img: 'assets/images/coin.webp'
+        },
+        {
+            name: 'helm',
+            img: 'assets/images/helm.webp'
+        },
+        {
+            name: 'helm',
+            img: 'assets/images/helm.webp'
+        },
+        {
+            name: 'map',
+            img: 'assets/images/map.webp'
+        },
+        {
+            name: 'map',
+            img: 'assets/images/map.webp'
+        },
+        {
+            name: 'pirate',
+            img: 'assets/images/pirate.webp'
+        },
+        {
+            name: 'pirate',
+            img: 'assets/images/pirate.webp'
+        },
+        {
+            name: 'skull',
+            img: 'assets/images/skull.webp'
+        },
+        {
+            name: 'skull',
+            img: 'assets/images/skull.webp'
+        },
+        {
+            name: 'treasure',
+            img: 'assets/images/skull.webp'
+        },
+        {
+            name: 'treasure',
+            img: 'assets/images/skull.webp'
+        },
+    ]
+})
