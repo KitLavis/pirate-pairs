0eb2bed0f31c5f8f2e95a94e36858a9a7f7da12c
KitLavis <>
2023-10-23 08:01:53 +0000

    JS: checkForMatch() function written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,98 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const cards = [
+    const cardArray = [
     {
         name: 'coin',
-        img: 'asset/images/coin.webp'
+        img: 'assets/images/coin.webp'
     },
     {
         name: 'coin',
-        img: 'asset/images/coin.webp'
+        img: 'assets/images/coin.webp'
     },
     {
         name: 'helm',
-        img: 'asset/images/helm.webp'
+        img: 'assets/images/helm.webp'
     },
     {
         name: 'helm',
-        img: 'asset/images/helm.webp'
+        img: 'assets/images/helm.webp'
     },
     {
         name: 'map',
-        img: 'asset/images/map.webp'
+        img: 'assets/images/map.webp'
     },
     {
         name: 'map',
-        img: 'asset/images/map.webp'
+        img: 'assets/images/map.webp'
     },
     {
         name: 'pirate',
-        img: 'asset/images/pirate.webp'
+        img: 'assets/images/pirate.webp'
     },
     {
         name: 'pirate',
-        img: 'asset/images/pirate.webp'
+        img: 'assets/images/pirate.webp'
     },
     {
         name: 'skull',
-        img: 'asset/images/skull.webp'
+        img: 'assets/images/skull.webp'
     },
     {
         name: 'skull',
-        img: 'asset/images/skull.webp'
+        img: 'assets/images/skull.webp'
     },
     {
         name: 'treasure',
-        img: 'asset/images/treasure.webp'
+        img: 'assets/images/treasure.webp'
     },
     {
         name: 'treasure',
-        img: 'asset/images/treasure.webp'
+        img: 'assets/images/treasure.webp'
     },
     ];
 
     const gameBoard = document.querySelector(".gameboard");
     var cardsChosen = [];
     var cardsChosenId = [];
+    var cardsWon = [];
 
     function generateCards() {
-        for (let i = 0; i < cards.length; i++) {
+        for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement("img");
             card.setAttribute("src", "assets/images/card_back.webp");
             card.setAttribute("data-id", i);
             card.setAttribute("class", "card");
-            // card.addEventListener("click", flipCard);
+            card.addEventListener("click", flipCard);
             gameBoard.appendChild(card);
         }
-    }
+    };
+
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img');
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute("src", "assets/images/blank.webp")
+            cards[optionTwoId].setAttribute("src", "assets/images/blank.webp")
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute("src", "assets/images/card_back.webp")
+            cards[optionTwoId].setAttribute("src", "assets/images/card_back.webp")
+        };
+        cardsChosen = [];
+        cardsChosenId = [];
+    };
 
     function flipCard() {
-        var cardID = this.getAttribute('data-id');
-        cardsChosen.push(cards[cardId].name);
+        var cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
         cardsChosenId.push(cardId);
         this.setAttribute("src", cardArray[cardId].img);
         if (cardsChosen.length === 2) {
             setTimeout(checkForMatch, 500);
         }
-    }
-
-    // function checkForMatch();
+    };
 
     generateCards();
 });
