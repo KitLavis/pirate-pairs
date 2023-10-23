2e8c053f918c2598dbaff24272586c9a991af801
KitLavis <>
2023-10-23 07:41:07 +0000

    JS: flipCard() function written

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ];
 
     const gameBoard = document.querySelector(".gameboard");
+    var cardsChosen = [];
+    var cardsChosenId = [];
 
     function generateCards() {
         for (let i = 0; i < cards.length; i++) {
@@ -63,6 +65,18 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(card);
         }
     }
+
+    function flipCard() {
+        var cardID = this.getAttribute('data-id');
+        cardsChosen.push(cards[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute("src", cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
+
+    // function checkForMatch();
 
     generateCards();
 });
